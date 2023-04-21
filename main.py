549dcb51b01010d1af7d96ed2f03817ea4fb78ae
Luca Cussino <lucacussino@hotmail.com>
2023-04-21 14:19:05 -0300

    metodo get

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.title = 'Mi aplicacin con FastAPI'
+app.version = '0.0.1'
 
-@app.get('/')
+movies = [
+    {
+    'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
+    }
+]
+
+
+@app.get('/',tags=['home'])
 def message():
-    return 'Hello world!'
+    return HTMLResponse('<h1> Hello world </h1>')
+
+@app.get('/movies',tags=['movies'])
+def get_movies():
+    return movies
